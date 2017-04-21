57ae077a1c51248a00e37d3ddad4d38dd93a1af1
AndyGreenwell <andy@juliacomputing.com>
2017-04-21 12:20:12 -0400

    Remove plot functionality and Winston dependency

--- src/ROC.jl
+++ src/ROC.jl
@@ -1,17 +1,14 @@
 module ROC
 
 	using DataArrays
-	using Winston
 
 	export	ROCData,
 		roc,
 		AUC,
 		PPV,
 		cutoffs,
-		plot
 
 	include("rocdata.jl")
 	include("roc_main.jl")
-	include("rocplot.jl")
 
 end # module
